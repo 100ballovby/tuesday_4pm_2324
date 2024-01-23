a92d7aab83381c930142c3bca3e5594c81a6fee6
greatraksin <greatraksin@icloud.com>
2024-01-23 17:31:38 +0300

    Adding functions lesson 1

---
+++ 19_functions_2301/01_primer1.py
@@ -0,0 +1,10 @@
+# определение функции say_hello
+def say_hello():
+    return 'Hello!'
+
+
+# вызов функции say_hello с возвратом значения в переменную greeting
+greeting = say_hello()
+
+# вывод результата работы функции
+print(greeting)
