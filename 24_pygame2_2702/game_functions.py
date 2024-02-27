8714014e17cfc601a760ad0d40797b923f3a7e54
greatraksin <greatraksin@icloud.com>
2024-02-27 17:45:58 +0300

    Refactoring game function

--- 24_pygame2_2702/game_functions.py
+++ 24_pygame2_2702/game_functions.py
@@ -1,17 +1,36 @@
 import random
 
 
+def reset_object_position(obj, screen_rect):
+	"""
+	Функция, которая сбрасывает позицию не игрового объекта
+	:param obj: не игровой объект
+	:param screen_rect: область квадрата экрана pygame
+	"""
+	obj.x = random.randint(50, screen_rect.right - 50)
+	obj.y = -20
+
+
+def check_screen_collision(obj, screen_rect):
+	return obj.bottom >= screen_rect.bottom
+
+
+def check_player_collision(obj, player):
+	return obj.colliderect(player)
+
+
 def non_playable_obj_move(obj, player, screen, speed_y):
 	obj.y += speed_y
 	screen_rect = screen.get_rect()  # получить "колижн модель" экрана
-	if obj.bottom >= screen_rect.bottom:
-		obj.x = random.randint(50, screen_rect.right - 50)
-		obj.y = -20
+	if check_screen_collision(obj, screen_rect):
+		reset_object_position(obj, screen_rect)
 		return 'miss'
-	elif obj.colliderect(player):
-		obj.x = random.randint(50, screen_rect.right - 50)
-		obj.y = -20
+
+	if check_player_collision(obj, player):
+		reset_object_position(obj, screen_rect)
 		return 'catch'
+
+	return None
 
 
 def player_motion(obj, screen, speed_x):
