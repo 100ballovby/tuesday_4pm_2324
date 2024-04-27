2f0ffe7b9d6d68ebd195eed66676109feed375c3
GreatRaksin <greatraksin@icloud.com>
2024-04-27 18:05:18 +0300

    Adding opponent motion function

--- 26_ping_pong_pygame/game.py
+++ 26_ping_pong_pygame/game.py
@@ -1,17 +1,30 @@
 import sys
+from random import choice
 
 import pygame as pg
 
 
+def round_restart(obj, s_width, s_height):
+	global speed_x, speed_y
+	obj.x = s_width // 2
+	obj.y = s_height // 2
+	speed_x *= choice([-1, 1])
+	speed_y *= choice([-1, 1])
+
+
 def ball_move(obj, s_width, s_height, play_obj, opp):
-	global speed_x, speed_y  # из функции можно менять значение этих переменных
+	global speed_x, speed_y, player_score, opponent_score  # из функции можно менять значение этих переменных
 	obj.x += speed_x
 	obj.y += speed_y
 
 	if obj.top <= 0 or obj.bottom >= s_height:
 		speed_y *= -1
-	elif obj.left <= 0 or obj.right >= s_width:
-		speed_x *= -1
+	elif obj.left <= 0:
+		player_score += 1
+		round_restart(obj, s_width, s_height)
+	elif obj.right >= s_width:
+		opponent_score += 1
+		round_restart(obj, s_width, s_height)
 	elif obj.colliderect(play_obj) or obj.colliderect(opp):
 		speed_x *= -1
 
@@ -36,9 +49,8 @@ def opponent_motion(obj, p_obj, s, win_h):
 		obj.bottom = win_h
 
 
-
-
 pg.init()  # это находится наверху и инициализирует библиотеку
+pg.font.init()
 
 # создаем экран игры
 W = 1280
@@ -70,7 +82,10 @@ speed = 7
 p_speed = 0
 o_speed = speed
 ball_moving = False
-speed_x = speed_y = speed
+speed_x = speed_y = speed * choice([-1, 1])
+player_score = 0
+opponent_score = 0
+score_font = pg.font.SysFont('comicsans', 64)
 
 pg.display.update()  # если на экране игры нужно что-то показать до начала игры
 while True:  # главный цикл игры
@@ -85,6 +100,12 @@ while True:  # главный цикл игры
 	pg.draw.rect(screen, VIOLET, opponent)
 	pg.draw.aaline(screen, WHITE, [W // 2, 0], [W // 2, H])
 	pg.draw.ellipse(screen, VIOLET, ball)
+
+	player_score_text = score_font.render(str(player_score), True, VIOLET)
+	opponent_score_text = score_font.render(str(opponent_score), True, VIOLET)
+
+	screen.blit(player_score_text, [W // 2 + 50, H // 2])
+	screen.blit(opponent_score_text, [W // 2 - 90, H // 2])
 
 	pg.display.update()  # должен оставаться последним из отображений
 
